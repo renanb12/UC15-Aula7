0c65f253b37b9e70dfb44a60276cd1e3f8358bb4
renanb12 <renanbernardo315@gmail.com>
2024-09-05 16:21:01 -0300

    Programa finalizado

--- Hoocks/src/screens/Login.tsx
+++ Hoocks/src/screens/Login.tsx
@@ -1,9 +1,42 @@
-
+import { useEffect, useState } from "react";
+import { Alert, Image } from "react-native";
+import {
+  Container,
+  ContainerButton,
+  ContainerIcon,
+  ContainerInputs,
+  Input,
+  Label,
+} from "./Styles";
 
 const Login = () => {
-    return (
+  const [username, setUsername] = useState("Email:");
+  const [password, setPassword] = useState("Senha:");
 
-    );
-}
+//   useEffect(() => {
+//     if (password && password.length > 3) setPassword("");
+//   });
+
+  const showLogedAlert = () => {
+    console.log("OIiiii");
+  };
+  return (
+    <Container>
+      <ContainerIcon>
+        <Image
+          style={{ width: 120, height: 120 }}
+          source={require("../../assets/image.png")}
+        />
+      </ContainerIcon>
+      <ContainerInputs>
+        <Input onChangeText={setUsername} value={username} />
+        <Input onChangeText={setPassword} value={password} />
+      </ContainerInputs>
+      <ContainerButton onPress={showLogedAlert}>
+        <Label>Entrar</Label>
+      </ContainerButton>
+    </Container>
+  );
+};
 
 export default Login;
--- Hoocks/src/screens/Styles.ts
+++ Hoocks/src/screens/Styles.ts
@@ -1,8 +1,52 @@
 import styled from "styled-components/native";
 
-const input = styled.TextInput`
-  height: 40px;
-  margin: 12px;
-  border-width: 1px;
+export const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #c2c2c2;
+  flex: 1;
+`;
+
+export const ContainerInputs = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerButton = styled.TouchableOpacity`
+  width: 89%;
+  height: 70px;
+  margin-top: 80px;
+  border-radius: 50px;
   padding: 10px;
+  justify-content: center;
+  align-items: center;
+  background-color: #262626;
+`;
+
+export const ContainerIcon = styled.View`
+  background-color: #D9D9D9;
+  height: 150px;
+  width: 150px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.TextInput`
+  height: 70px;
+  width: 89%;
+  margin: 12px;
+  border-radius: 50px;
+  padding: 10px;
+  background-color: #fff;
+`;
+
+export const Label = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 22px;
 `;
